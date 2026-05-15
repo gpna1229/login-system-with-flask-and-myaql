b393dd22ef74b5b4a0283e8dbca29910fb1a4b9b
gpna1229 <annaduen@gmail.com>
2026-05-15 14:11:47 +0800

    Add sqlalchemy settings

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-from flask import Flask
+import os
+
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+from models import db, User
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
+db.init_app(app)
+
+@app.route("/users")
+def user_list():
+    users = db.session.execute(db.select(User).order_by(User.username)).scalars()
+    return render_template("list.html", users=users)
+
+app.run()
