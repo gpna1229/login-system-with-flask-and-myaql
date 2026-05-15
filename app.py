c3936f5a81476a5295e5fcd8045df9e523859b53
gpna1229 <annaduen@gmail.com>
2026-05-15 18:26:16 +0800

    Complete registration feature and redirect to user list

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
 
 from models import db, User
@@ -11,9 +11,22 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 db.init_app(app)
 
-@app.route("/users")
+@app.route("/userlist")
 def user_list():
     users = db.session.execute(db.select(User).order_by(User.username)).scalars().all()
     return render_template("list.html", users=users)
+
+@app.route("/register", methods=["GET", "POST"])
+def user_create():
+    if request.method == "POST":
+        user = User(
+            username=request.form.get("username"),
+            password = request.form.get("password"),
+            email=request.form.get("email"),
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("user_list"))
+    return render_template("register.html")
 
 app.run()
